3c0fb22d605f87962ea9717b90f29d78c5f5fa0e
atille888 <atille88@gmail.com>
2024-04-15 17:45:12 -0400

    Progress_1
    yay

--- atiller3_main_exercise_1.py
+++ atiller3_main_exercise_1.py
@@ -1,1 +1,11 @@
-#yippee
+# Exercise 1
+### Name: Alana Chandler
+### JHID: atiller3
+### language: Python
+
+# Take a look at the video generated, labeled by frame with slider
+import atiller3_problem_1_exercise_1
+
+
+import atiller3_problem_2_exercise_1
+
---
+++ atiller3_problem_1_exercise_1.py
@@ -0,0 +1,7 @@
+import numpy as np
+import imageio as io
+
+# Load the video
+mov = "TEST_MOVIE_00001-small-motion.tif"
+# mov_frames=io.imread(mov)
+print(!pwd)

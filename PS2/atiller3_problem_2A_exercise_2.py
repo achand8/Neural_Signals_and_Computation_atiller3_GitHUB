96b49ed1fdcddbcb3a96cd2823428cd083268853
atille888 <atille88@gmail.com>
2024-05-10 21:19:02 -0400

    Progress 3

--- PS2/atiller3_problem_2A_exercise_2.py
+++ PS2/atiller3_problem_2A_exercise_2.py
@@ -3,6 +3,7 @@
 # Load tools
 import numpy as np
 import scipy.io as sio
+import scipy
 import matplotlib
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -28,49 +29,36 @@ def plot_2a(y,title):
     fig.tight_layout()
     plt.show()
 
-def plot_2a_gp(y, title):
-    plt.rc(('xtick', 'ytick'), labelsize=10)
-    fig, ax = plt.subplots(3, 2, figsize=(12, 8));
-    axs = ax.ravel()
-    [(axs[i].plot(np.arange(len(y.T)), y[i, :].T),
-      axs[i].set_title(f'Neuron {i + 1}', fontsize=10))
-     for i in np.arange(6)]
-    matplotlib.rcParams.update({'font.size': 10})
-    fig.suptitle(title)
-    fig.tight_layout()
-    plt.show()
 
-
-# Perform GP smoothing
 def gp_smoothing(A, l, x):
     '''
     :param A: float, scalar hyperparameter of GP estimate
     :param l: float, length parameter, hyperparameter of GP estimate
-    :param x: 1-d numpy array, data to fit to GP
-    :return gp_smoooth: 1-d array, GP fit for x
+    :param x: 1-d numpy array, data whose timepoints are used to fit to GP
+    :return gp_smooth: 1-d array, GP fit for x
     '''
-    t = x.astype('int64')
-    d = t-t[:,np.newaxis]
-    K = A * np.exp(-np.square(d)/l)
+    t=np.expand_dims(np.linspace(0, len(x), 400), 1)/1000
+    d = scipy.spatial.distance.cdist(t, t, 'sqeuclidean')
+    K = A * np.exp(-d/l)     # compute kernel
     invK = np.linalg.pinv(K)
 
     # Fit the GP for predictive distribution such that
     # mu = KT Kinv x
     # cov = K - KT K-1 K
-    mu = K.T.dot(invK).dot(t)
+    mu = K.T.dot(invK).dot(x)
     cov = K - K.T.dot(invK).dot(K)
     gp_smooth=np.random.multivariate_normal(mean=
-                                            mu.ravel(),
-                                            cov=cov,size=5)
-    gp_smooth=gp_smooth.mean(axis=0)
+                                            mu,
+                                            cov=cov,size=1)
+    # gp_smooth[gp_smooth<0] = 0  # limit gp to whole numbers
     return gp_smooth.T
 
 
 # Plot the PSTH and some GP-smoothed PSTHs
-# plot_2a(psth,'PSTH'); A=2; l=0.2
-# gp_smooth = np.array([gp_smoothing(A=A, l=l, x=psth[i,:]) for i in np.arange(psth.shape[0])]).reshape(psth.shape)
-# plot_2a(gp_smooth,f'PSTH - GP smoothing at A={A}, l={l}')
-# # Try far-out parameters for A and l
-# A=2; l=20; gp_smooth = np.array([gp_smoothing(A=A, l=l, x=psth[i,:]) for i in np.arange(psth.shape[0])]).reshape(psth.shape)
-# plot_2a(gp_smooth,f'PSTH - GP smoothing at A={A}, l={l}')
-
+# plot_2a(psth,'PSTH')
+A=1.0; l=2e-3
+gp_smooth = np.array([gp_smoothing(A=A, l=l, x=psth[i,:]) for i in np.arange(psth.shape[0])]).reshape(psth.shape)
+plot_2a(gp_smooth,f'PSTH - GP smoothing at A={A}, l={l}')
+# Try out parameters for A and l - looks smoother
+A=4.0; l=5e-2; gp_smooth = np.array([gp_smoothing(A=A, l=l, x=psth[i,:]) for i in np.arange(psth.shape[0])]).reshape(psth.shape)
+plot_2a(gp_smooth,f'PSTH - GP smoothing at A={A}, l={l}')
---
+++ PS2/atiller3_problem_2B_exercise_2.py
@@ -0,0 +1,7 @@
+# Problem Set 2, #2A=B
+
+# Load tools
+import numpy as np
+import scipy.io as sio
+import matplotlib.pyplot as plt
+from atiller3_problem_2A_exercise_2 import *

bcaf86812846802cd6d6c22e56a3156177d8d4c8
atille888 <atille88@gmail.com>
2024-05-11 09:24:20 -0400

    Progress 6

--- PS2/atiller3_problem_2B_exercise_2.py
+++ PS2/atiller3_problem_2B_exercise_2.py
@@ -1,4 +1,4 @@
-# Problem Set 2, #2A=B
+# Problem Set 2, #2B
 
 # Load tools
 import numpy as np
@@ -12,9 +12,34 @@ from atiller3_problem_2A_exercise_2 import *
 ndat = sio.loadmat('sample_dat.mat')['dat']
 dfn = pd.DataFrame(ndat[0])
 spk_arr = np.array(dfn['spikes'][:])
-psth = spk_arr.sum(axis=0)
+psth = spk_arr.sum(axis=0) # Generate PSTH
 
+# Generate GP-smoothed PSTH
 gp_smooth = np.array([gp_smoothing(A=1.0,l=2e-3,x=psth[i,:]) for i in np.arange(psth.shape[0])])
-print(psth.shape)
-print(gp_smooth.shape)
-# psth = psth.mean(axis=1)
+
+# Perform PCA on PSTH
+psth_X = psth - psth.mean(axis=1,keepdims=True)
+pca_psth = pca(n_components=psth.shape[0]).fit(psth_X)
+psth_evs = pca_psth.components_[0:3,:]
+
+# Perform PCA on smoothed PSTH
+gpsth_X = gp_smooth - gp_smooth.mean(axis=1,keepdims=True)
+pca_gpsth = pca(n_components=gp_smooth.shape[0]).fit(gpsth_X)
+gpsth_evs = pca_gpsth.components_[0:3,:]
+
+def pc3d(x,x_sm,titles):
+    '''
+    Plot 2 3-dim ndarrays representing PCs side-by-side.
+    :param x: ndarray of dims 3xN, left plot
+    :param x_sm: ndarray of dims 3xN, right plot
+    :param titles: list of 2 strings, become titles for subplots
+    :return:
+    '''
+    f = plt.figure(figsize=(12, 5))
+    ax = f.add_subplot(1, 2, 1, projection='3d');
+    ax.plot(x[0,:], x[1,:], x[2,:], 'c'); ax.set_title(f'{titles[0]}')
+    ax.view_init(azim=-35, elev=30); ax.set_xlabel('PC1'); ax.set_ylabel('PC2'); ax.set_zlabel('PC3')
+    ax2 = f.add_subplot(1, 2, 2, projection='3d'); ax2.view_init(azim=-35, elev=30)
+    ax2.plot(x_sm[0,:], x_sm[1,:], x_sm[2,:], 'c'); ax2.set_title(f'{titles[1]}')
+    ax2.set_xlabel('PC1'); ax2.set_ylabel('PC2'); ax2.set_zlabel('PC3')
+    plt.show()
--- PS2/atiller3_problem_2C_exercise_2.py
+++ PS2/atiller3_problem_2C_exercise_2.py
@@ -4,6 +4,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import pkg_resources
+import scipy.io as sio
+import pip
+
+# Load the N x T data
+ndat = sio.loadmat('sample_dat.mat')['dat']
+dfn = pd.DataFrame(ndat[0])
+spk_arr = np.array(dfn['spikes'][:])
 
 # Load Gpy for Gaussian Processes
 packages = [package.project_name for package in pkg_resources.working_set]
@@ -12,38 +20,61 @@ from elephant.gpfa import GPFA
 import quantities as pq
 import neo as n
 
+def gen_spk_train(spk_arr):
+    """
+    Generate a spike train for use in neo.SpikeTrain
+    :param spk_arr: ndarray of dims trials x neurons x time
+    :return spk_train: neo Spike Train object
+    """
+    spk_train = []
+    for trial in spk_arr:
+        spk_trial = []
+        for neuron in trial:
+            ntimes = np.where(neuron==1)[0] * pq.ms
+            spk_trial.append(n.SpikeTrain(ntimes,t_stop=len(neuron)*pq.ms))
+        spk_train.append(spk_trial)
+    return spk_train
+
+def plot_spk_train(spk_train,trial):
+    '''
+    Plot spike train for a specified trial.
+    :param spk_train: neo SpikeTrain object
+    :param trial: int, trial number to plot in [1, total number of trials]
+    :return:
+    '''
+    for i, spiketrain in enumerate(spk_train[trial-1]):
+        plt.plot(spiketrain, np.ones(len(spiketrain)) * i, ls='', marker='|')
+    plt.title(f'Spike trains from trial {trial}')
+    plt.tight_layout()
+    plt.show()
 
 
-spk_train = []
-for trial in spk_arr:
-    spk_trial = []
-    for neuron in trial:
-        ntimes = np.where(neuron==1)[0] * pq.ms
-        spk_trial.append(n.SpikeTrain(ntimes,t_stop=len(neuron)*pq.ms))
-    spk_train.append(spk_trial)
-
-for i, spiketrain in enumerate(spk_train[0]):
-    plt.plot(spiketrain, np.ones(len(spiketrain)) * i, ls='', marker='|')
-
-plt.tight_layout()
-plt.show()
-
-bin_size = 1 * pq.ms
-latent_dimensionality = 3
-gpfa_3dim = GPFA(bin_size=bin_size,x_dim=latent_dimensionality)
-trajs = gpfa_3dim.fit_transform(spk_train)
-
-f = plt.figure(figsize=(15,5))
-ax = f.add_subplot(1, 1, 1, projection='3d')
-[ax.plot(trajs[i][0],trajs[i][1],trajs[i][2],'c') for i in np.arange(len(trajs))]
-avg_trajs=np.mean(trajs,axis=0,'k')
-ax.view_init(azim=-100, elev=0)
-ax.set_xlim3d(-0.05,0.05)
-ax.set_ylim3d(-0.07,0.06)
-ax.set_zlim3d(-0.02,0.8)
-plt.tight_layout()
-plt.show()
+def perform_gpfa(spk_train, bin_size=1 * pq.ms, latent_dimensionality=3):
+    """
+    Compute gpfa trajectories of a neo SpikeTrain object.
+    :param spk_train: neo SpikeTrain object
+    :param bin_size: quantities object, time bin width
+    :param latent_dimensionality: dims of lower-dimensional trajectory
+    :return trajs: ndarray, computed GPFA trajectories
+    """
+    gpfa_3dim = GPFA(bin_size=bin_size,x_dim=latent_dimensionality)
+    trajs = gpfa_3dim.fit_transform(spk_train)
+    return trajs
 
 
-dfn = pd.DataFrame(ndat[0])
-spk_arr = np.array(dfn['spikes'][:])
+def plot_trajs(trajs):
+    f = plt.figure(figsize=(6,5))
+    ax = f.add_subplot(1, 1, 1, projection='3d'); c = 'rcbyg'
+    [ax.plot(trajs[i][0],trajs[i][1],trajs[i][2],
+             c[i%len(c)]) for i in np.arange(len(trajs))]
+    avg_trajs=np.mean(trajs,axis=0); print(avg_trajs.shape)
+    ax.plot(avg_trajs[0,:],avg_trajs[1,:],avg_trajs[2,:],c='k',
+            linewidth=5.0, label='avg')
+    ax.set_title('GPFA per-trial trajectories')
+    ax.legend(loc='best', fontsize=10)
+    ax.view_init(azim=-133, elev=41)
+    ax.set_xlabel('x');
+    ax.set_ylabel('y');
+    ax.set_zlabel('z')
+    plt.tight_layout()
+    plt.show()
